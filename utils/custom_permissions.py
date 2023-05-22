bd1a9000cf3080e72b0a02bd59a6049ebb80c3ed
elijah <popoolakejiah@gmail.com>
2023-05-22 02:07:49 +0100

    update: user_type choice was added to the user model

--- utils/custom_permissions.py
+++ utils/custom_permissions.py
@@ -17,3 +17,48 @@ class IsGetRequestOrAuthenticated(BasePermission):
             return True
         else:
             return bool(request.user and request.user.is_authenticated)
+
+
+class CanAccessPublicationNews(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        else:
+            return request.user.user_type in ["publication_news"]
+
+
+class CanAccessEventTraining(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        else:
+            return request.user.user_type in ["event_training"]
+
+
+class CanAccessPublicView(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        else:
+            return request.user.user_type in ["public_view"]
+
+
+class CanAccessRegistrationPayment(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        else:
+            return request.user.user_type in ["registrations_payments"]
+
+
+class CanAccessProspectiveCertificates(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        else:
+            return request.user.user_type in ["prospective_certificates"]
