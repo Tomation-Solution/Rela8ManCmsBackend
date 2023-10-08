46c1e86a3848857c94e7851eeb79a4db5a1d4fae
Nwokolo Matthew <markothedevmail@gmail.com>
2023-10-08 14:13:02 -0700

    allowed the slider image  to be optional

--- homepage/models.py
+++ homepage/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class HomePageSlider(models.Model):
     title = models.CharField(max_length=200)
     content= models.TextField()
-    banner = models.ImageField()
+    banner = models.ImageField(null=True,upload_to='sldierbanner/%d/',default=None)
 
     def __str__(self):
         return self.title
